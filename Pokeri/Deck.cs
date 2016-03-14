0f83a2fdbc1db4faeebbeb25c1827239a8a3fd81
Timo Heinonen <h8262@student.jamk.fi>
2016-03-14 23:44:03 +0200

    asd

---
+++ Pokeri/Deck.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pokeri
+{
+    class Deck
+    {
+        List<Card> deck;
+
+        public Deck()
+        {
+            deck = new List<Card>();
+        }
+
+
+        public void BuildDeck(Card card)
+        {
+            Random rng = new Random();
+            for(int i = 0; i == 51; i++)
+            {
+                Card card1 = new Card();
+                int suit = rng.Next(1, 5);
+                int number = rng.Next(1, 14);
+
+
+            }
+
+            /*
+            Card card1 = new Card { Suit = 0, Number = 1 };
+            deck.Add(card1);
+            Card card2 = new Card { Suit = 0, Number = 2 };
+            deck.Add(card2);
+            Card card3 = new Card { Suit = 0, Number = 3 };
+            deck.Add(card3);       */ 
+        }
+
+        
+    public void ScrambleDeck ()
+        {
+            int i = deck.Count;
+                               
+        }
+    }
+}
